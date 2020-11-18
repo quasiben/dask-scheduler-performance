796b287495b3a4d5c4048c8f8fb784fdb4f0f5a0
John Kirkham <jakirkham@gmail.com>
2020-11-18 12:28:03 -0800

    Repeat computation for 20 iterations

--- shuffle.py
+++ shuffle.py
@@ -7,7 +7,9 @@ if __name__ == "__main__":
     client = Client("127.0.0.1:8786")
     ddf_h = timeseries(start='2000-01-01', end='2000-01-02', partition_freq='1min')
     result = shuffle(ddf_h, "id", shuffle="tasks")
-    ddf = client.persist(result)
-    _ = wait(ddf)
+    for i in range(20):
+        ddf = client.persist(result)
+        _ = wait(ddf)
+        client.cancel(ddf)
     client.shutdown()
     time.sleep(0.5)
